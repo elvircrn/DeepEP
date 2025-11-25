dff7de848404fa0fe742ff81aca8585bb1b6724a
Elvir Crncevic <elvircrn@gmail.com>
2025-11-25 15:18:37 +0100

    open_mem_handle

--- csrc/deep_ep.cpp
+++ csrc/deep_ep.cpp
@@ -114,7 +114,7 @@ void SharedMemoryAllocator::get_mem_handle(MemHandle* mem_handle, void* ptr) {
 }
 
 void SharedMemoryAllocator::open_mem_handle(void** ptr, MemHandle* mem_handle) {
-    printf("============= enable_fabric = %d =============\n", int(enable_fabric));
+    printf("============= open_mem_handle: enable_fabric = %d =============\n", int(enable_fabric));
 
     if (enable_fabric) {
         size_t size = mem_handle->size;
