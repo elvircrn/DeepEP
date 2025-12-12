55ce075d525d12c3963b42df91b29fd30c77b86e
Elvir Crncevic <elvircrn@gmail.com>
2025-12-12 16:33:19 +0100

    Log server response code

--- csrc/kernels/launch.cuh
+++ csrc/kernels/launch.cuh
@@ -53,7 +53,6 @@ cfg.dynamicSmemBytes = smem_size;
         case 4: case_macro(4); \
         case 8: case_macro(8); \
         case 16: case_macro(16); \
-        case 24: case_macro(24); \
         default: EP_HOST_ASSERT(false and "Unsupported ranks"); \
     } while (false)
 
@@ -75,7 +74,6 @@ cfg.dynamicSmemBytes = smem_size;
         case 4: case_macro(dtype, 4); \
         case 8: case_macro(dtype, 8); \
         case 16: case_macro(dtype, 16); \
-        case 24: case_macro(dtype, 24); \
         default: EP_HOST_ASSERT(false && "Unsupported ranks"); \
     } while (false)
 
