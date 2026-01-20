7be2edf1e1ea27ed78285e9e6bcc7abc14851f2c
Elvir Crncevic <elvircrn@gmail.com>
2026-01-20 10:54:20 +0100

    try bringing back lcuda

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ if __name__ == '__main__':
             include_dirs.append(cccl_path)
         library_dirs.extend([f'{nvshmem_dir}/lib'])
         nvcc_dlink.extend(['-dlink', f'-L{nvshmem_dir}/lib', '-lnvshmem_device'])
-        extra_link_args.extend([f'-l:{nvshmem_host_lib}', '-l:libnvshmem_device.a', f'-Wl,-rpath,{nvshmem_dir}/lib'])
+        extra_link_args.extend([f'-l:{nvshmem_host_lib}', '-l:libnvshmem_device.a', f'-Wl,-rpath,{nvshmem_dir}/lib', '-lcuda'])
 
     if int(os.getenv('DISABLE_SM90_FEATURES', 0)):
         # Prefer A100
