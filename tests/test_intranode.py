797143d91e73fb0cbf7b73584b36130dcf9427f1
Elvir Crnčević <elvircrn@gmail.com>
2026-01-30 19:14:51 +0100

    Update test_intranode.py

--- tests/test_intranode.py
+++ tests/test_intranode.py
@@ -233,7 +233,7 @@ def test_main(num_sms: int, local_rank: int, num_ranks: int, rank: int, buffer: 
 # noinspection PyUnboundLocalVariable
 def test_loop(local_rank: int, num_local_ranks: int):
     rank, num_ranks, group = init_dist(local_rank, num_local_ranks)
-    test_ll_compatibility, num_rdma_bytes = False, 0
+    test_ll_compatibility, num_rdma_bytes = True, 0
     if test_ll_compatibility:
         ll_num_tokens, ll_hidden, ll_num_experts, ll_num_topk = 16, 5120, 256, 9
         num_rdma_bytes = deep_ep.Buffer.get_low_latency_rdma_size_hint(ll_num_tokens, ll_hidden, num_ranks, ll_num_experts)
